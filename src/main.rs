9c6b7b71b380e25a2b95df7c189004f7faf40324
tsukiiru <108170925+tsukiiru@users.noreply.github.com>
2026-08-18 14:34:59 +0700

    goog: fix flickering

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,8 @@ use termion::raw::IntoRawMode;
 use termion::terminal_size;
 use tokio::sync::oneshot;
 
-#[allow(dead_code)]
 const ESC_CODE: &str = "\x1b";
-#[allow(dead_code)]
 const CLEAR_CODE: &str = "[0m";
-
-#[allow(dead_code)]
-const MODE_CODE: [i8; 8] = [
-    1, // bold
-    2, // dim
-    3, // italic
-    4, // underline
-    5, // blinking
-    7, // reverse
-    8, // hidden
-    9, // strikethrough
-];
 
 #[allow(dead_code)]
 const FG_COLOR_CODE: [i8; 8] = [
@@ -37,48 +23,43 @@ const FG_COLOR_CODE: [i8; 8] = [
     37, // white
 ];
 
-#[allow(dead_code)]
-const BG_COLOR_CODE: [i8; 8] = [
-    40, // black
-    41, // red
-    42, // green
-    43, // yellow
-    44, // blue
-    45, // magenta
-    46, // cyan
-    47, // white
-];
-
 const GOOG: &str = include_str!("./goog-ascii-art.txt");
 const SPEED: i16 = 1;
 const WAIT: u64 = 50;
 
-struct Vec2(i16, i16);
+struct IVec2(i16, i16);
+struct UVec2(u16, u16);
 
-impl Vec2 {
+impl IVec2 {
     fn new(x: i16, y: i16) -> Self {
-        Vec2(x, y)
+        IVec2(x, y)
+    }
+}
+
+impl UVec2 {
+    fn new(x: u16, y: u16) -> Self {
+        UVec2(x, y)
     }
 }
 
 struct Goog {
-    pos: Vec2,
-    size: Vec2,
-    velocity_mul: Vec2,
+    pos: UVec2,
+    size: UVec2,
+    velocity_mul: IVec2,
     speed: i16,
 }
 
 impl Goog {
     fn new() -> Self {
         Goog {
-            pos: Vec2::new(1, 1),    // from top left corner
-            size: Vec2::new(50, 24), // width, height in characters count of the ascii art
-            velocity_mul: Vec2::new(1, 1),
+            pos: UVec2::new(1, 1),    // from top left corner
+            size: UVec2::new(50, 24), // width, height in characters count of the ascii art
+            velocity_mul: IVec2::new(1, 1),
             speed: SPEED,
         }
     }
 
-    fn is_collide(&self, screen_size: &Vec2) -> Collision {
+    fn is_collide(&self, screen_size: &UVec2) -> Collision {
         let (mut h_collide, mut v_collide) = (None, None);
 
         if self.pos.0 <= 1 {
@@ -113,8 +94,8 @@ impl Goog {
     }
 
     fn apply_velocity(&mut self) {
-        self.pos.0 += self.speed * self.velocity_mul.0;
-        self.pos.1 += self.speed * self.velocity_mul.1;
+        self.pos.0 = (self.pos.0 as i16 + self.speed * self.velocity_mul.0) as u16;
+        self.pos.1 = (self.pos.1 as i16 + self.speed * self.velocity_mul.1) as u16;
     }
 }
 
@@ -167,26 +148,21 @@ async fn main() {
         write!(stdout, "\x1b[?2026h").unwrap();
         stdout.flush().unwrap();
 
-        for (i, _) in GOOG.lines().enumerate() {
+        for i in 0..(goog.size.1 + 2) {
             write!(
                 stdout,
-                "{}{}{}{}",
-                termion::cursor::Goto(goog.pos.0 as u16, goog.pos.1 as u16 + i as u16),
-                termion::clear::BeforeCursor,
-                termion::cursor::Goto(
-                    goog.pos.0 as u16 + goog.size.0 as u16,
-                    goog.pos.1 as u16 + i as u16
-                ),
-                termion::clear::AfterCursor,
+                "{}{}",
+                termion::cursor::Goto(1, goog.pos.1 + i - 1),
+                termion::clear::CurrentLine,
             )
             .unwrap();
-        }
+        } // only clearing the using lines
 
         for (i, l) in GOOG.lines().enumerate() {
             write!(
                 stdout,
                 "{}",
-                termion::cursor::Goto(goog.pos.0 as u16, goog.pos.1 as u16 + i as u16),
+                termion::cursor::Goto(goog.pos.0, goog.pos.1 + i as u16),
             )
             .unwrap();
             write!(
@@ -201,7 +177,7 @@ async fn main() {
         stdout.flush().unwrap();
 
         let screen_size = terminal_size().unwrap();
-        let screen_size = Vec2::new(screen_size.0 as i16, screen_size.1 as i16);
+        let screen_size = UVec2::new(screen_size.0, screen_size.1);
         let collision = goog.is_collide(&screen_size);
 
         if collision.0.is_some() || collision.1.is_some() {
